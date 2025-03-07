b294df6025fc6db21801d25da3ae27b935663306
chumwaters <alextangalov@hotmail.com>
2025-03-07 23:22:05 +0200

    Added implementation of generateRandomChar() and generateRandom(Address&)

--- tasks/set1/solutions/task4/Address.cpp
+++ tasks/set1/solutions/task4/Address.cpp
@@ -1,25 +1,50 @@
 #include "Address.h"
 
-void readAddress(Address& a) {
-	std::cout << "Enter city name: " << '\n';
-	std::cin.getline(a.city, 32);
-	std::cout << "Enter postal code: " << '\n';
-	std::cin >> a.postCode;
-	std::cout << "Enter street name: " << '\n';
-	std::cin.getline(a.street, 64);
-	std::cout << "Enter street number: " << '\n';
-	std::cin >> a.streetNumber;
-	std::cout << "Enter street character: " << '\n';
-	std::cin >> a.streetCharacter;
-	std::cout << "Enter building entrance: " << '\n';
-	std::cin >> a.entrance;
-	std::cout << "Enter floor number: " << '\n';
-	std::cin >> a.floor;
-	std::cout << "Enter apartment number: " << '\n';
-	std::cin >> a.apartment;
+void read(Address& a) {
+    std::cout << "Enter city: ";
+    std::cin.getline(a.city, 32);
 
+    std::cout << "Enter postal code: ";
+    std::cin >> a.postCode;
+    std::cin.ignore();
+
+    std::cout << "Enter street: ";
+    std::cin.getline(a.street, 64);
+
+    std::cout << "Enter street number: ";
+    std::cin >> a.streetNumber;
+
+    std::cout << "Enter street character: ";
+    std::cin >> a.streetCharacter;
+
+    std::cout << "Enter entrance: ";
+    std::cin >> a.entrance;
+
+    std::cout << "Enter floor: ";
+    std::cin >> a.floor;
+
+    std::cout << "Enter apartment: ";
+    std::cin >> a.apartment;
 }
 
-void printAddress(const Address& a) {
+void print(const Address& a) {
 	std::cout << "\"" << a.city << "\"" << ", " << a.postCode << ", " << "\"" << "ul. " << a.street << "\"" << ", " << a.streetNumber << ' ' << a.streetCharacter << ", " << "vh. " << a.entrance << ", et." << a.floor << ", ap." << a.apartment << '\n';
+}
+
+char generateRandomChar() {
+    return 'A' + rand() % 26;
+}
+
+void generateRandom(Address& a) {
+    const char* cities[] = { "Plovdiv", "Ruse", "Burgas", "Varna", "Sofia" };
+    const char* streets[] = { "Hristo Botev", "Ivan Vazov", "Tsar Simeon", "Vasil Levski", "Slavyanska" };
+
+    strcpy_s(a.city, cities[rand() % (sizeof(cities) / sizeof(const char*))]);
+    a.postCode = 1000 + rand() % 9000;
+    strcpy_s(a.street, streets[rand() % (sizeof(streets) / sizeof(const char*))]);
+    a.streetNumber = 1 + rand() % 10;
+    a.streetCharacter = generateRandomChar();
+    a.entrance = rand() % 10;
+    a.floor = rand() % 10;
+    a.apartment = rand() % 50;
 }
