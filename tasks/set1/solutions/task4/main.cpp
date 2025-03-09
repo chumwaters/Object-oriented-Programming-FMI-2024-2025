96f2eef50d0f1c9242dea968e8a29dbafb5aea05
chumwaters <alextangalov@hotmail.com>
2025-03-09 17:36:44 +0200

    Added demonstration for the usage of generateRandom(Student&), print(const Student&), calcAverageScore(const Student&) and filterAboveThreshold(Student*, int, int&, unsigned short)

--- tasks/set1/solutions/task4/main.cpp
+++ tasks/set1/solutions/task4/main.cpp
@@ -1,4 +1,40 @@
+#include "Student.h"
+
 int main() {
 
+	Student s1;
+	Student s2;
+	Student s3;
+	generateRandom(s1);
+	generateRandom(s2);
+	generateRandom(s3);
+	print(s1);
+	std::cout << "\nAverage score : " << calcAverageScore(s1);
+	std::cout << "\n\n";
+	print(s2);
+	std::cout << "\nAverage score : " << calcAverageScore(s2);
+	std::cout << "\n\n";
+	print(s3);
+	std::cout << "\nAverage score : " << calcAverageScore(s3);
+
+	int size = 3;
+	Student students[3] = { s1, s2, s3 };
+
+	int filteredSize = 0;
+	unsigned short threshold = 5;
+	Student* filtered = filterAboveThreshold(students, size, filteredSize, threshold);
+
+	std::cout << "\n\nStudents with average grade above " << threshold << ":\n";
+	std::cout << "------------------------------------\n";
+	for (int i = 0; i < filteredSize; i++) {
+		print(filtered[i]);
+		std::cout << "\n\n";;
+	}
+
+	delete[] filtered;
+	clear(s1);
+	clear(s2);
+	clear(s3);
+	
 	return 0;
 }
