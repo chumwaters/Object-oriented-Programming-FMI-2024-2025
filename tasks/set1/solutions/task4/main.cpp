1ab2642f7682a7e6fe2d08332ac3f1e976bb6166
chumwaters <alextangalov@hotmail.com>
2025-03-28 17:45:51 +0200

    Commented out old tests and added new ones for the readFromFile and writeToFile functions

--- tasks/set1/solutions/task4/main.cpp
+++ tasks/set1/solutions/task4/main.cpp
@@ -1,8 +1,34 @@
 #include "Student.h"
 
+#include <iostream>
+#include <fstream>
+
 int main() {
 
-	Student s1;
+	Student s;
+
+	std::ifstream file("test.txt");
+	if (!file) {
+		std::cerr << "Failed to open file for reading!\n";
+		return 1;
+	}
+
+	readFromFile(s, file);
+	print(s);
+
+	file.close();
+
+	std::ofstream outFile("testOut.txt");
+	if (!file) {
+		std::cerr << "Failed to open file for writing!\n";
+		return 1;
+	}
+
+	writeToFile(s, outFile);
+
+	outFile.close();
+
+	/*Student s1;
 	Student s2;
 	Student s3;
 	generateRandom(s1);
@@ -35,6 +61,7 @@ int main() {
 	clear(s1);
 	clear(s2);
 	clear(s3);
-	
+	*/
+
 	return 0;
 }
