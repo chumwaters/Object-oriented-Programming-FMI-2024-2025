da43824359c79cb0364f25d09088dc88cb147682
chumwaters <alextangalov@hotmail.com>
2025-04-10 19:16:28 +0300

    Added declarations and definitions for bool validateDate(unsigned int, unsigned int, int) and bool setDate(unsigned int, unsigned int, int) methods along with declaration and definition of bool isLeapYear(int) function

---
+++ tasks/set3/task1/Date.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Date {
+private:
+	unsigned int day;
+	unsigned int month;
+	int year;
+
+public:
+	bool validateDate(unsigned int day, unsigned int month, int year);
+	bool isLeapYear(int year);
+	bool setDate(unsigned int day, unsigned int month, int year);
+};
