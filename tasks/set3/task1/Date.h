a09d66ee08a56b9936ee7ce1c4739d03cd68481d
chumwaters <alextangalov@hotmail.com>
2025-04-10 19:34:58 +0300

    Added declaration of bool read() method

--- tasks/set3/task1/Date.h
+++ tasks/set3/task1/Date.h
@@ -10,4 +10,5 @@ public:
 	bool validateDate(unsigned int day, unsigned int month, int year);
 	bool isLeapYear(int year);
 	bool setDate(unsigned int day, unsigned int month, int year);
+	bool read();
 };
