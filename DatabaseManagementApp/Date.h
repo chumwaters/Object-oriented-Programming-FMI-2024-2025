3517f0271f5668a53cdd981be39d0956efca2cc6
chumwaters <alextangalov@hotmail.com>
2025-05-20 00:00:39 +0300

    Added declaration of daysInMonth(int m, int y) static method.

--- DatabaseManagementApp/Date.h
+++ DatabaseManagementApp/Date.h
@@ -5,5 +5,9 @@ struct Date {
 	int month = 1;
 	int year  = 1900;
 
+	//-----------------------------------
+	// Helpers
+	//-----------------------------------
 	static bool isLeapYear(int y);
+	static int daysInMonth(int m, int y);
 };
