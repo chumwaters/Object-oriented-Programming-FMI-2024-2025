3dde802d5521c849fc580005b651f864021b33be
chumwaters <alextangalov@hotmail.com>
2025-05-20 00:00:53 +0300

    Added definition of daysInMonth(int m, int y) static method.

--- DatabaseManagementApp/Date.cpp
+++ DatabaseManagementApp/Date.cpp
@@ -3,3 +3,11 @@
 bool Date::isLeapYear(int y) {
 	return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
 }
+
+int Date::daysInMonth(int m, int y) {
+	static const int days[12] = { 31, 28, 31,30,31,30,31,31,30,31,30,31 };
+	
+	if (m == 2 && isLeapYear(y)) return 29;
+	
+	return days[m - 1];
+}
