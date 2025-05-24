616662b251df60f83f86dab700dcf00cb91022e3
chumwaters <alextangalov@hotmail.com>
2025-05-24 03:52:28 +0300

    Added definition of addColumn(const std::string&, DataType) method.

--- DatabaseManagementApp/Table.cpp
+++ DatabaseManagementApp/Table.cpp
@@ -1,3 +1,10 @@
 #include "Table.h"
 
 Table::Table(const std::string& tableName) : name(tableName) {}
+
+void Table::addColumn(const std::string& colName, DataType type) {
+	columns.push_back(Column(colName, type));
+	for (Row& row : rows) {
+		row.cells.push_back(CellValue());
+	}
+}
