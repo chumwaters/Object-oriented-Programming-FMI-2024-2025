e1885982a4c4ca628c592493303364f1630313fb
chumwaters <alextangalov@hotmail.com>
2025-05-24 03:29:23 +0300

    Added definitions and inline declarations of property-accessor methods.

--- DatabaseManagementApp/Table.h
+++ DatabaseManagementApp/Table.h
@@ -18,4 +18,11 @@ public:
 	/// @brief Constructs a table with a given name.
 	/// @param Name to give to constructed Table.
 	Table(const std::string& tableName);
+
+	//---------------------------------------
+	// Accessors
+	//---------------------------------------
+	const std::string& getName() const { return name; };
+	size_t columnCount() const { return columns.size(); };
+	size_t rowCount() const { return rows.size(); };
 };
