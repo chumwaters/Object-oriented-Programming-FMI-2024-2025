8818c9652949bda75e8fdc601f001da3a17f5809
chumwaters <alextangalov@hotmail.com>
2025-05-28 01:43:51 +0300

    Added declaration and documentation of showTables() method.

--- DatabaseManagementApp/Database.h
+++ DatabaseManagementApp/Database.h
@@ -11,6 +11,10 @@ private:
 	std::vector<Table> tables;
 
 public:
+	//--------------------------------------
+	// Operations
+	//--------------------------------------
+
 	/// @brief Adds a new table to the DB.
 	/// @param t Table to be added to the DB.
 	void addTable(const Table& t);
@@ -18,6 +22,19 @@ public:
 	//---------------------------------------
 	// Accessors
 	//---------------------------------------
+	
+	/// @brief Returns pointer to a table by name,
+	/// or nullptr if not found.
 	Table* getTable(const std::string& name);
+
+	/// @brief Returns const pointer to a table by name,
+	/// or nullptr if not found.
 	const Table* getTable(const std::string& name) const;
+
+	// --------------------------------------
+	//	Information
+	// --------------------------------------
+
+	/// @brief Prints names of all tables in DB.
+	void showTables() const;
 };
