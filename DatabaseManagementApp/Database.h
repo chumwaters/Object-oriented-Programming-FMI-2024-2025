bc82a7f29402304f03e7a92b25f10dfe2bc54a6f
chumwaters <alextangalov@hotmail.com>
2025-05-28 01:36:54 +0300

    Added declarations for const and non-const accessors getTable(const std::string& name).

--- DatabaseManagementApp/Database.h
+++ DatabaseManagementApp/Database.h
@@ -14,4 +14,10 @@ public:
 	/// @brief Adds a new table to the DB.
 	/// @param t Table to be added to the DB.
 	void addTable(const Table& t);
+
+	//---------------------------------------
+	// Accessors
+	//---------------------------------------
+	Table* getTable(const std::string& name);
+	const Table* getTable(const std::string& name) const;
 };
