3965e6b470be07b57c6d0e4b296091161af9b578
chumwaters <alextangalov@hotmail.com>
2025-05-29 08:26:58 +0300

    Added documentation for CommandHandler(Database& db) ctor.

--- DatabaseManagementApp/CommandHandler.h
+++ DatabaseManagementApp/CommandHandler.h
@@ -7,5 +7,9 @@ private:
 	Database& db;
 
 public:
+	/// @brief CommandHandler constructor from Database reference.
+	/// @param db Database reference to construct CommandHandler object from.
 	CommandHandler(Database& db);
+
+
 };
