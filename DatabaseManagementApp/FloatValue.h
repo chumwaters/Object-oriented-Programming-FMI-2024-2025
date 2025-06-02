d15585161fe093b45648f5e9d83d5d581b3da1ec
chumwaters <alextangalov@hotmail.com>
2025-06-02 14:19:39 +0300

    Created FloatValue class as a public CellValue and declared its properties and c-tor.

---
+++ DatabaseManagementApp/FloatValue.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CellValue.h"
+
+/// @brief Represents a floating-point value in a table cell.
+class FloatValue : public CellValue {
+private:
+	double value;
+
+public:
+	/// @brief Constructs a FloatValue from a given double value.
+	/// @param v The value to store.
+	FloatValue(double v) : value(v) {}
+};
