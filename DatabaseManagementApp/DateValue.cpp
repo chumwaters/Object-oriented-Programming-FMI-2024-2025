c25bc4ecf0521168caff6b11060b97d2755b73a5
chumwaters <alextangalov@hotmail.com>
2025-06-02 18:16:38 +0300

    Added definition of bool DateValue::convertTO(DataType target, CellValue*& out) const override method.

--- DatabaseManagementApp/DateValue.cpp
+++ DatabaseManagementApp/DateValue.cpp
@@ -1,4 +1,5 @@
 #include "DateValue.h"
+#include "StringValue.h"
 
 bool DateValue::equals(const CellValue* other) const {
 	if (other->getType() != DataType::DATE) return false;
@@ -6,4 +7,17 @@ bool DateValue::equals(const CellValue* other) const {
 	const DateValue* dv = static_cast<const DateValue*>(other);
 
 	return value == dv->value;
+}
+
+bool DateValue::convertTo(DataType target, CellValue*& out) const {
+	switch (target) {
+		case DataType::DATE:
+			out = new DateValue(value);
+			return true;
+		case DataType::STRING:
+			out = new StringValue(value.toString());
+			return true;
+		default:
+			return false;
+	}
 }
