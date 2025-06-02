fef8fb03130aba6cdfec28f76475f210f4be1694
chumwaters <alextangalov@hotmail.com>
2025-06-02 18:24:28 +0300

    Added declarations, documentations and inline definitions for methods DataType NullValue::getType() const override and std::string NullValue::toString() const override.

--- DatabaseManagementApp/NullValue.h
+++ DatabaseManagementApp/NullValue.h
@@ -2,8 +2,15 @@
 
 #include "CellValue.h"
 
+/// @brief Represents a NULL (empty) value in a table cell.
 class NullValue : public CellValue {
 public:
-	/// @brief Constructs a NullValue;
+	/// @brief Constructs a NullValue.
 	NullValue() = default;
+
+	/// @return DataType::NULLTYPE
+	DataType getType() const override { return DataType::NULLTYPE; }
+
+	/// @return The string "NULL".
+	std::string toString() const override {	return "NULL"; };
 };
