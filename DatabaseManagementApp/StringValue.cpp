e4a9149385b40e15de6e6b45c6488b0208aba049
chumwaters <alextangalov@hotmail.com>
2025-06-02 15:20:07 +0300

    Added definition of bool StringValue::equals(const CellValue* other) const override method.

---
+++ DatabaseManagementApp/StringValue.cpp
@@ -0,0 +1,22 @@
+#include "StringValue.h"
+
+std::string StringValue::toString() const {
+	std::string out = "\"";
+
+	for (char c : value) {
+		if (c == '"' || c == '\\') out += '\\';
+		out += c;
+	}
+
+	out += '"';
+
+	return out;
+}
+
+bool StringValue::equals(const CellValue* other) const {
+	if (other->getType() != DataType::STRING) return false;
+
+	const StringValue* sv = static_cast<const StringValue*>(other);
+
+	return value == sv->value;
+}
