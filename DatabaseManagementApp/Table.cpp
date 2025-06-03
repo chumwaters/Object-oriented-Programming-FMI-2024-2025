9e17af03c04491ba380288f700ace6f18d8cbf49
chumwaters <alextangalov@hotmail.com>
2025-06-03 19:52:54 +0300

    Added definition and documentation of static helper method printRowsFormatted and used it in definition of print() method.

--- DatabaseManagementApp/Table.cpp
+++ DatabaseManagementApp/Table.cpp
@@ -5,6 +5,9 @@
 #include "StringValue.h"
 #include "NullValue.h"
 
+/// @brief Unescapes C++-style string literals.
+/// @param val string literal to unescape.
+/// @return Unescaped modification of val parameter.
 static std::string unescapeString(const std::string& val) {
 	std::string parsed;
 
@@ -25,6 +28,55 @@ static std::string unescapeString(const std::string& val) {
 	}
 
 	return parsed;
+}
+
+/// @brief Prints a collection of Rows with a common column structure to standart output with aligned
+/// columns.
+/// @param columns The metadata describing each column (names and types).
+/// @param rows The rows of data to print. These must match the column structure.
+static void printRowsFormatted(const std::vector<Column>& columns, const std::vector<Row>& rows) {
+	std::vector<std::size_t> widths(columns.size(), 0);
+
+	// Finding the maximum entry size in each column and storing in widths
+	for (std::size_t c = 0; c < columns.size(); ++c) {
+		std::size_t maxLen = columns[c].name.length();
+
+		for (std::size_t r = 0; r < rows.size(); ++r) {
+			std::size_t len = rows[r][c]->toString().length();
+
+			if (len > maxLen) maxLen = len;
+		}
+	}
+
+	// Printing header
+	for (std::size_t i = 0; i < columns.size(); ++i) {
+		std::cout << "| " << columns[i].name;
+
+		std::size_t padding = widths[i] > columns[i].name.size() ? widths[i] - columns[i].name.size() : 0;
+		for (std::size_t p = 0; p < padding; ++p) std::cout << ' ';
+		if (i + 1 < columns.size()) std::cout << " | ";
+	}
+	std::cout << "|\n";
+
+	// Printing appropriate number of dashes for each collumn to form the 
+	// line on the second row of the visualization
+	for (std::size_t i = 0; i < columns.size(); ++i) {
+		for (std::size_t j = 0; j < widths[i]; ++j) std::cout << '-';
+		if (i + 1 < columns.size()) std::cout << "-+-";
+	}
+	std::cout << "\n";
+
+	// Printing rows
+	for (std::size_t r = 0; r < rows.size(); ++r) {
+		for (std::size_t c = 0; c < columns.size(); ++c) {
+			std::string cellStr = rows[r][c]->toString();
+			std::cout << "| " << cellStr;
+			std::size_t padding = widths[c] > cellStr.size() ? widths[c] - cellStr.size() : 0;
+
+			for (std::size_t p = 0; p < padding; ++p) std::cout << ' ';
+		}
+		std::cout << "|\n";
+	}
 }
 
 Table::Table(const std::string& tableName) : name(tableName) {}
@@ -183,4 +235,8 @@ void Table::describe() const {
 
 		std::cout << ")\n";
 	}
+}
+
+void Table::print() const {
+	printRowsFormatted(columns, rows);
 }
