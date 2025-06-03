0b4b96132d530efde1e6af05626a3b8c8355cd1b
chumwaters <alextangalov@hotmail.com>
2025-06-03 19:54:39 +0300

    Removed definitions of static helper method printRowsFormatted and printTable method.

--- DatabaseManagementApp/Database.cpp
+++ DatabaseManagementApp/Database.cpp
@@ -2,61 +2,6 @@
 
 #include <exception>
 
-/// @brief Prints a collection of Rows with a common column structure to standart output with aligned
-/// columns.
-/// @param columns The metadata describing each column (names and types).
-/// @param rows The rows of data to print. These must match the column structure.
-static void printRowsFormatted(const std::vector<Column>& columns, const std::vector<Row>& rows) {
-	std::vector<std::size_t> widths(columns.size(), 0);
-
-	// Initializing widths array with collumn names
-	for (std::size_t i = 0; i < columns.size(); ++i) {
-		widths[i] = columns[i].name.size();
-	}
-
-	// Iterating through each element in each row in search
-	// for the longest element of each column 
-	for (const Row& row : rows) {
-		for (std::size_t i = 0; i < row.size(); ++i) {
-			std::size_t len = row[i].toString().size();
-			if (len > widths[i]) widths[i] = len;
-		}
-	}
-
-	// Printing header
-	for (std::size_t i = 0; i < columns.size(); ++i) {
-		std::cout << columns[i].name;
-
-		std::size_t padding = widths[i] > columns[i].name.size() ? widths[i] - columns[i].name.size() : 0;
-		for (std::size_t p = 0; p < padding; ++p) std::cout << ' ';
-		if (i + 1 < columns.size()) std::cout << " | ";
-	}
-	std::cout << "\n";
-
-	// Printing appropriate number of dashes for each collumn to form the 
-	// line on the second row of the visualization
-	for (std::size_t i = 0; i < columns.size(); ++i) {
-		for (std::size_t j = 0; j < widths[i]; ++j) std::cout << '-';
-		if (i + 1 < columns.size()) std::cout << "-+-";
-	}
-	std::cout << "\n";
-
-	// Printing rows
-	for (std::size_t r = 0; r < rows.size(); ++r) {
-		const Row& row = rows[r];
-
-		for (std::size_t i = 0; i < row.size(); ++i) {
-			std::string cellStr = row[i].toString();
-			std::cout << cellStr;
-			std::size_t padding = widths[i] > cellStr.size() ? widths[i] - cellStr.size() : 0;
-
-			for (std::size_t p = 0; p < padding; ++p) std::cout << ' ';
-			if (i + 1 < row.size()) std::cout << " | ";
-		}
-		std::cout << "\n";
-	}
-}
-
 void Database::addTable(const Table& t) {
 	for (const Table& tbl : tables) {
 		if (tbl.getName() == t.getName()) {
@@ -82,16 +27,6 @@ const Table* Database::getTable(const std::string& name) const {
 	}
 
 	return nullptr;
-}
-
-void Database::printTable(const std::string& tableName) const {
-	const Table* table = getTable(tableName);
-	if (!table) {
-		std::cerr << "Error: Table '" << tableName << "' not found.\n";
-		return;
-	}
-
-	printRowsFormatted(table->getColumns(), table->getRows());
 }
 
 void Database::selectMatchingRows(const std::string& tableName, std::size_t columnIndex,
