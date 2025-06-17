9f4ea7d45194eb3c56772ccbd60d1961a47deb26
chumwaters <alextangalov@hotmail.com>
2025-06-17 20:42:58 +0300

    Added missing new line in exit message for dialogue mode in printRowsFormatted().

--- DatabaseManagementApp/TablePrintUtils.h
+++ DatabaseManagementApp/TablePrintUtils.h
@@ -119,7 +119,7 @@ static void printRowsFormatted(const std::vector<Column>& columns, const std::ve
 				validCommand = true;
 			}
 			else if (input == "exit") {
-				std::cout << "Exiting dialogue mode.";
+				std::cout << "Exiting dialogue mode.\n";
 				return;
 			}
 			else {
