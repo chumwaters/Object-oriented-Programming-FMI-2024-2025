f0d0625ba13a0b703eff0044ad949db609d2c170
chumwaters <alextangalov@hotmail.com>
2025-06-18 20:34:19 +0300

    Removed unnecessary code from FLOAT case in fromString() method.

--- DatabaseManagementApp/CellValue.cpp
+++ DatabaseManagementApp/CellValue.cpp
@@ -13,12 +13,7 @@ CellValue* CellValue::fromString(const std::string& str, DataType type, bool& ok
 	try {
 		switch (type) {
 			case DataType::INT: return new IntValue(std::stoll(str));
-			case DataType::FLOAT: {
-				char buff[32];
-				std::snprintf(buff, sizeof(buff), "%.2f", std::stod(str));
-			
-				return new FloatValue(std::stod(str));
-			}
+			case DataType::FLOAT: return new FloatValue(std::stod(str));
 			case DataType::DATE: {
 				bool valid = false;
 				Date d = Date::fromString(str, valid);
