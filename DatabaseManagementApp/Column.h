eaae22632094a96b18c58cab3c6ef20ab66e7715
chumwaters <alextangalov@hotmail.com>
2025-06-20 21:22:55 +0300

    Added documentation and Column class and methods.

--- DatabaseManagementApp/Column.h
+++ DatabaseManagementApp/Column.h
@@ -4,10 +4,16 @@
 
 #include <string>
 
+/// @brief Represents a column in a table with a name and data type.
 struct Column {
 	std::string name;
 	DataType type;
 
+	/// @brief Default constructor for Column class.
 	Column() = default;
-	Column(const std::string& n, DataType t) : name(n), type(t) {};
+
+	/// @brief Constructs a Column with from a given name and data type.
+	/// @param name Name to give to the constructed Column.
+	/// @param type Data type for the constructed Column.
+	Column(const std::string& name, DataType type) : name(name), type(type) {};
 };
